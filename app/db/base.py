a391eebc45534c324a8e79b3d15f2d3d8640811e
jiayist <jiayi.lim.2020@scis.smu.edu.sg>
2022-10-03 22:28:22 +0800

    insert jobrole, jobroleskill,courseskill db

--- app/db/base.py
+++ app/db/base.py
@@ -3,3 +3,7 @@
 from app.db.base_class import Base  # noqa
 from app.models.role import Role  # noqa
 from app.models.staff import Staff  # noqa
+from app.models.jobrole import Jobrole  # noqa
+from app.models.jobroleskill import Jobroleskill  # noqa
+from app.models.courseskill import Courseskill  # noqa
+
