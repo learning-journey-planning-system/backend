eae60c2ddb59d8b0d14f568864793f75f13989df
jiayist <jiayi.lim.2020@scis.smu.edu.sg>
2022-10-04 22:02:14 +0800

    get + remove - jobroleskill courseskill

--- app/crud/crud_jobroleskill.py
+++ app/crud/crud_jobroleskill.py
@@ -1,5 +1,26 @@
-from .base import CRUDBase
+from typing import Optional, List
+
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
 from app.models.jobroleskill import JobRoleSkill
 from app.schemas.jobroleskill import JobRoleSkillCreate, JobRoleSkillUpdate
 
-jobroleskill = CRUDBase[JobRoleSkill, JobRoleSkillCreate, JobRoleSkillUpdate](JobRoleSkill)
+
+class CRUDJobRoleSkill(CRUDBase[JobRoleSkill, JobRoleSkillCreate, JobRoleSkillUpdate]):
+
+    #Get Skill from Jobrole
+    def get(self, db: Session, jobrole_id: int, skill_id: str) -> Optional[JobRoleSkill]:
+            return (db.query(self.model)
+            .filter(self.model.jobrole_id == jobrole_id)
+            .filter(self.model.skill_id == skill_id)
+            .first()
+        )
+
+    def remove(self, db: Session, *, jobrole_id: int, skill_id: str) -> List[JobRoleSkill]:
+        obj = db.query(self.model).get(jobrole_id, skill_id)
+        db.delete(obj)
+        db.commit()
+        return db.query(self.model).all()    
+
+jobrole = CRUDJobRoleSkill(JobRoleSkill)
