34f17267c4208ffb988e687ba3382a7e8f9b14f8
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2022-10-07 15:26:37 +0800

    add cors origin *

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
+        allow_origins=["*"],
         allow_methods=["*"],
         allow_headers=["*"],
     )
